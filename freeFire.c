74c153be2577b57053723e697bb8bfaef5247da5
Joao Victor <joaovic2012@hotmail.com>
2025-11-27 13:14:05 -0300

    Funcao inserir e mostrar itens criada

--- freeFire.c
+++ freeFire.c
@@ -11,8 +11,27 @@ typedef struct {
 } Item;
 
 //Funcoes
-void inserirItem(){
+void inserirItens(Item *itens, int quantidade){
+    //Valida se tem menos de 11 itens
+    if(quantidade < 10){
+        //Cadastrar o item em memoria
+        for(int i = 0; i < quantidade; i++){
+            printf("\nItem %d:\n", i + 1);
 
+            //leitor do nome
+            printf("Nome: ");
+            scanf("%29s", itens[i].nome);
+
+            //leitor da tipo
+            printf("tipo: ");
+            scanf("%19s", itens[i].tipo);
+
+            //leitor de quantidade
+            printf("quantidade: ");
+            scanf("%d", &itens[i].quantidade);
+        }
+
+    }
 }
 
 void removerItem(){
@@ -25,4 +44,35 @@ void listarItens(){
 
 void buscarItem(){
 
+}
+
+void mostrarItens(Item *itens, int quantidade){
+    if(quantidade < 10){
+        //mostrar dados
+        printf("\n--- Dados dos Itens ---\n");
+
+        //ler todos os dados de de cada posicao do vetor
+        for (int i = 0; i < quantidade; i++) {
+            printf("Item %d : -- nome: %s -- tipo:  %s -- %d quantidade --\n",
+                i + 1,
+                itens[i].nome,
+                itens[i].tipo,
+                itens[i].quantidade
+            );
+        }
+    }
+}
+
+int main(){
+
+    int quantidade;
+    printf("Quantos Itens deseja cadastrar?");
+    scanf("%d", &quantidade);
+
+    Item *itens = malloc(quantidade * sizeof(Item));
+
+    cadastrarItens(itens,quantidade);
+    mostrarItens(itens,quantidade);
+
+    return 0;
 }
