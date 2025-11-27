154f0bf742af333fd26c4fac3165e43859306b9a
Joao Victor <joaovic2012@hotmail.com>
2025-11-27 12:31:14 -0300

    estrutura criada

---
+++ freeFire.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Definicao de struct
+
+typedef struct {
+    char nome[30];
+    char tipo[20];
+    int quantidade;
+} Item;
+
+//Funcoes
+void inserirItem(){
+
+}
+
+void removerItem(){
+
+}
+
+void listarItens(){
+
+}
+
+void buscarItem(){
+
+}
